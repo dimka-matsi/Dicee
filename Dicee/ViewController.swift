4313f2c15bdce5ef95a38e6befc9d419ee429df3
Dmitry Matskello <dimka_matsi@hotmail.com>
2019-06-10 11:40:47 +0300

    init and create app

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Dmitry Matskello on 6/10/19.
+//  Copyright © 2019 Dmitry Matskello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
