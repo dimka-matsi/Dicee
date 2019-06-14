93d2575b0cd94ba7d5faf2545bbb84eb1088b544
Dmitry Matskello <dimka_matsi@hotmail.com>
2019-06-14 12:33:31 +0300

    add functional to dicee

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -19,6 +24,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
+        print(randomDiceIndex2)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
     }
     
 }
